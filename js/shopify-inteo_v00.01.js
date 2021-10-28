fb48692bb96b6e42240e63f1d538d14e8bc69e24
crimeandroid <crimeandroid@gmail.com>
2021-10-28 13:42:36 +0200

    the first example to get script attrs

--- js/shopify-inteo_v00.01.js
+++ js/shopify-inteo_v00.01.js
@@ -1,9 +1,13 @@
 function loadDoc() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("demo").innerHTML =
-    this.responseText;
-  }
-  xhttp.open("GET", "ajax_info.txt");
-  xhttp.send();
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("demo").innerHTML = this.responseText;
+        const currentScript = document.getElementById('shopify-inteo');
+        console.log(currentScript);
+        const args = currentScript.dataset.args.split(',');
+        // const args = currentScript.getAttribute('data-args').split(',');
+        document.getElementById("args").innerHTML = args[0];
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
 }
